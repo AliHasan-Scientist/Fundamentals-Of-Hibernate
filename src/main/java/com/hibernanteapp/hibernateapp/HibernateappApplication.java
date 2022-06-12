5d11abb699f868bb2bd740f74634893c3cc10f45
Roy Ali Hassn <royalihasan0987@gmail.com>
2022-06-12 18:32:05 +0100

    add entities in myapp

---
+++ src/main/java/com/hibernanteapp/hibernateapp/HibernateappApplication.java
@@ -0,0 +1,14 @@
+package com.hibernanteapp.hibernateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@AutoConfiguration
+@SpringBootApplication
+public class HibernateappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hibernanteapp/hibernateapp/HibernateappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernanteapp.hibernateapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
